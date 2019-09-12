105c8418c360ca2b1f4a3788161e4684967d0ee8
LHunn <leyla.hunn@outlook.com>
2019-09-12 17:36:41 +0100

    New classes

---
+++ src/main/java/simpleslickgame/BlobfishCharacter.java
@@ -0,0 +1,4 @@
+package simpleslickgame;
+
+public class BlobfishCharacter {
+}
---
+++ src/main/java/simpleslickgame/NinjaCharacter.java
@@ -0,0 +1,25 @@
+package simpleslickgame;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class NinjaCharacter {
+
+    public NinjaCharacter() {
+    }
+
+    public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
+
+    }
+
+    public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
+
+    }
+
+    public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {
+
+    }
+
+}
