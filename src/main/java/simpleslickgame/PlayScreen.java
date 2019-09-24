0191616ab48342a456f1fe6f4f22d70b8ef71c8f
LHunn <leyla.hunn@outlook.com>
2019-09-24 12:31:50 +0100

    Updates

--- src/main/java/simpleslickgame/PlayScreen.java
+++ src/main/java/simpleslickgame/PlayScreen.java
@@ -8,16 +8,16 @@ import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
 import java.awt.*;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Random;
-import java.lang.InterruptedException;
-
-import static java.lang.Thread.interrupted;
 
 public class PlayScreen extends BasicGameState {
 
     Image background;
+    Image backgroundHitbox;
     Image character;
     Image enemy;
     int characterX = 650;
@@ -32,17 +32,28 @@ public class PlayScreen extends BasicGameState {
 
     private SpriteSheet blobfishSpritesheet;
     private Animation blobfishAnimation;
+    private Animation blobfishAnimation1;
+    private Animation blobfishAnimation2;
+
 
     public PlayScreen(int state) {
     }
 
     public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
         background = new Image("C:\\Users\\lhunn\\IdeaProjects\\Slick2D\\src\\main\\resources\\image.png");
+        backgroundHitbox = new Image("C:\\Users\\lhunn\\IdeaProjects\\Slick2D\\src\\main\\resources\\image.png");
         character = new Image("C:\\Users\\lhunn\\IdeaProjects\\Slick2D\\src\\main\\resources\\ninja-two.png");
         enemy = new Image("C:\\Users\\lhunn\\IdeaProjects\\Slick2D\\src\\main\\resources\\enemy.png");
 
         blobfishSpritesheet = new SpriteSheet("sprite_sheet.png", 300, 128);
         blobfishAnimation = new Animation(blobfishSpritesheet, 300);
+        blobfishAnimation1 = new Animation(blobfishSpritesheet, 300);
+        blobfishAnimation2 = new Animation(blobfishSpritesheet, 300);
+
+        Image blobThree = new Image("blob-three.gif");
+
+        blobfishAnimation.addFrame(blobThree, 10);
+
 
         Image[] ninjaLeft = {new Image("ninja-two.png"), new Image("ninja-two.png")};
         Image[] ninjaUp = {new Image("ninja-one.png"), new Image("ninja-one.png")};
@@ -52,7 +63,7 @@ public class PlayScreen extends BasicGameState {
         movingLeft = new Animation(ninjaLeft, duration, false);
         movingRight = new Animation(ninjaRight, duration, false);
         movingUp = new Animation(ninjaUp, duration, false);
-        attack = new Animation(ninjaJump, duration, false);
+        attack = new Animation(ninjaJump, duration, true);
         movingLeft.setAutoUpdate(true);
         ninja = movingLeft;
 
@@ -60,6 +71,7 @@ public class PlayScreen extends BasicGameState {
         Random enemyY = new Random();
         RandomX = enemyX.nextInt(1300);
         RandomY = enemyY.nextInt(800);
+
     }
 
     public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
@@ -75,28 +87,53 @@ public class PlayScreen extends BasicGameState {
         enemyHitbox.setLocation(RandomX, RandomY);
         enemyHitbox.getLocation();
 
-        /*LinkedList<Animation> list = new LinkedList<Animation>();
-        list.add(blobfishAnimation);
-        list.add(blobfishAnimation);
+        //background 'hitbox' covering the enemy when it gets hit
+        Rectangle backgroundHitbox = new Rectangle(RandomX, RandomY, 140, 120);
+        backgroundHitbox.setLocation(RandomX,RandomY);
+        backgroundHitbox.getLocation();
 
+        LinkedList<Animation> list = new LinkedList<Animation>();
+
+        list.add(blobfishAnimation);
+        list.add(blobfishAnimation1);
+        list.add(blobfishAnimation2);
         Iterator<Animation> iterator = list.iterator();
-        while(iterator.hasNext()){ //returns true if there are more elements in LinkedList
-            iterator.next();
-            blobfishAnimation.draw(200, 200);
-        }*/
+        //while (iterator.hasNext()){ //returns true if there are more elements in LinkedList
 
+        if(ninja == attack && ninjaHitbox.intersects(enemyHitbox)){
+            blobfishAnimation.stop();
+            background.draw(RandomX,RandomY);
+            ninja.draw(characterX,characterY);
+        }
+
+        if (blobfishAnimation.isStopped()) {
+            background.draw(RandomX,RandomY);
+            g.drawAnimation(blobfishAnimation1,360, 694);
+            blobfishAnimation.draw(RandomX,RandomY,Color.red);
+            ninja.draw(characterX,characterY);
+        }
+
+            //g.drawAnimation(blobfishAnimation, 400,300);
+           // g.drawAnimation(blobfishAnimation1, 695,283);
+           // g.drawAnimation(blobfishAnimation2, 220, 103);
 
         //instead of a list, add a for loop to loop through the blobfish at random locations,
+
+
+        //Array[] squareArray = new Array[20];
+
+        /*for(int i = 0; i <= squareArray.length; i++) {
+            if (ninja == attack && ninjaHitbox.intersects(enemyHitbox)) {
+                blobfishAnimation.draw(700, 700);
+            }
+        }*/
 
         if(ninja != attack) {
             if (!ninjaHitbox.intersects(enemyHitbox)) {
                 blobfishAnimation.draw(RandomX, RandomY);
             }
         }
-        else if (ninja == attack && ninjaHitbox.intersects(enemyHitbox)) {
-                blobfishAnimation.draw(300, 300);
-            //using a sleep thread is a noooo. Possible for loop?
-        }
+
         else if (ninja == attack && !ninjaHitbox.intersects(enemyHitbox)) {
             blobfishAnimation.draw(RandomX,RandomY);
         }
@@ -149,12 +186,12 @@ public class PlayScreen extends BasicGameState {
         }
         if (input.isKeyPressed(Input.KEY_SPACE)) {
             ninja = attack;
-            }
-            return;
         }
-
-        public int getID () {
-            return 1;
-        }
-
+        return;
     }
+
+    public int getID () {
+        return 1;
+    }
+
+}
